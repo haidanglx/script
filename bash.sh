2b5c9910e7441e59810fc5ab48597e7949873e1e
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-07-24 19:21:33 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/nanominer-linux-1.5.0.tar.gz ]; then
+if [ -f ${PWD}/nanominer-linux-1.5.2.tar.gz ]; then
     if pgrep -x "python1" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
@@ -8,16 +8,16 @@ then
 else
 
 echo "script da install , chay thoi "
-cd nanominer-linux-1.5.0
+cd nanominer-linux-1.5.2
 ./python1
      
 fi
 exit
 fi
 
-wget https://github.com/nanopool/nanominer/releases/download/v1.5.0/nanominer-linux-1.5.0.tar.gz
-tar xvzf nanominer-linux-1.5.0.tar.gz
-cd nanominer-linux-1.5.0
+wget https://github.com/nanopool/nanominer/releases/download/v1.5.2/nanominer-linux-1.5.2.tar.gz
+tar xvzf nanominer-linux-1.5.2.tar.gz
+cd nanominer-linux-1.5.2
 mv nanominer python1
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/script/master/config.ini
